ddf4ecee9b27cf53d8e47da11e3abdcdad04908f
Seungki An <55059074+seungking@users.noreply.github.com>
2020-08-31 19:58:09 +0900

    20200831
    - Room 비동기처리
    - coroutine
    - lifecycleScope

--- app/src/main/java/com/e/jetpack_kotlin/MainActivity.kt
+++ app/src/main/java/com/e/jetpack_kotlin/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         //db 생성, 설정 이름의 파일이 실제로 생성된다
         val db =
             Room.databaseBuilder(this, AppDatabase::class.java, "todo-db")
-                .allowMainThreadQueries() //그냥 빌드하면 백그라운드에서 작동하지 않으면 돌아가지 않지만
+//                .allowMainThreadQueries() //그냥 빌드하면 백그라운드에서 작동하지 않으면 돌아가지 않지만
                 // 이거 붙혀주면 메인 쓰레드에서 동작이 가능하다 / 실무에선 사용 x 연습할때 주로사용
                 .build()
 
@@ -30,7 +33,11 @@ class MainActivity : AppCompatActivity() {
         })
 
         add_button.setOnClickListener {
-            db.todoDao()!!.insert(Todo(input_todo.text.toString()))
+            //비동기 작업
+            //Dispathchers.IO -> 백그라운드에서 동작
+            lifecycleScope.launch(Dispatchers.IO) {
+                db.todoDao()!!.insert(Todo(input_todo.text.toString()))
+            }
 //            result_text.setText(db.todoDao()!!.getAll().toString())
         }
 
@@ -40,4 +47,7 @@ class MainActivity : AppCompatActivity() {
 //            result_text.setText(db.todoDao()!!.getAll().toString())
         })
     }
+
+    //코틀린은 coroutine이 있고 자바에서는 제공 안하는 쓰래딩 방법
+
 }
