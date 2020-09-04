dd090b8996d5a6a6f58fd3d4e7066998655e99d8
Seungki An <55059074+seungking@users.noreply.github.com>
2020-09-04 20:00:33 +0900

    20200904
    - ViewModel 분리

--- app/src/main/java/com/e/jetpack_kotlin/MainActivity.kt
+++ app/src/main/java/com/e/jetpack_kotlin/MainActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
 import androidx.lifecycle.lifecycleScope
-import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -16,18 +17,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        //db 생성, 설정 이름의 파일이 실제로 생성된다
-        val db =
-            Room.databaseBuilder(this, AppDatabase::class.java, "todo-db")
-//                .allowMainThreadQueries() //그냥 빌드하면 백그라운드에서 작동하지 않으면 돌아가지 않지만
-                // 이거 붙혀주면 메인 쓰레드에서 동작이 가능하다 / 실무에선 사용 x 연습할때 주로사용
-                .build()
-
+        val viewModel =
+            ViewModelProvider(this, AndroidViewModelFactory(application)).get(
+                MainViewModel::class.java
+            )
 
         //UI 갱신
         //livedata observe
-        db.todoDao()!!.getAll()!!.observe(this, Observer {
+        viewModel.getAll()!!.observe(this, Observer {
             result_text.text = it.toString();
 
         })
@@ -36,15 +33,18 @@ class MainActivity : AppCompatActivity() {
             //비동기 작업
             //Dispathchers.IO -> 백그라운드에서 동작
             lifecycleScope.launch(Dispatchers.IO) {
-                db.todoDao()!!.insert(Todo(input_todo.text.toString()))
+                viewModel.insert(Todo(input_todo.text.toString()))
             }
 //            result_text.setText(db.todoDao()!!.getAll().toString())
         }
 
         //삭제 - title 같은 todo 찾아서 삭제
         delete_button.setOnClickListener(View.OnClickListener { v: View? ->
-            db.todoDao()!!.delete(db.todoDao()!!.get(input_todo.getText().toString()))
+
+            lifecycleScope.launch(Dispatchers.IO) {
+                viewModel.delete(input_todo.getText().toString())
 //            result_text.setText(db.todoDao()!!.getAll().toString())
+            }
         })
     }
 
---
+++ app/src/main/java/com/e/jetpack_kotlin/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.e.jetpack_kotlin
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+
+class MainViewModel(application: Application): AndroidViewModel(application) {
+
+    //db 생성, 설정 이름의 파일이 실제로 생성된다
+    val db =
+        Room.databaseBuilder(application, AppDatabase::class.java, "todo-db")
+//                .allowMainThreadQueries() //그냥 빌드하면 백그라운드에서 작동하지 않으면 돌아가지 않지만
+            // 이거 붙혀주면 메인 쓰레드에서 동작이 가능하다 / 실무에선 사용 x 연습할때 주로사용
+            .build()
+
+    fun getAll(): LiveData<List<Todo?>>? {
+        return db.todoDao()!!.getAll()
+    }
+
+    fun insert(todo:Todo){
+        return db.todoDao()!!.insert(todo)
+    }
+
+    fun delete(todo:String){
+        return db.todoDao()!!.insert(db.todoDao()!!.get(todo))
+    }
+
+}
